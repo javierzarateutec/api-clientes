94a625f2d32fe01ee0fcefb02cba99420fa9831b
javierzarateutec <javier.zarate@utec.edu.pe>
2025-07-04 02:13:19 -0500

    Add files via upload

--- BuscarClientes.py
+++ BuscarClientes.py
@@ -3,19 +3,20 @@ from boto3.dynamodb.conditions import Key
 import json
 
 def lambda_handler(event, context):
-    # Parsear el body JSON que viene como string
-    body = json.loads(event['body'])  # ✅ esto convierte el string en un dict
-    cliente_id = body['cliente_id']   # ✅ ahora puedes acceder al campo
+    body = event["body"]
+    if isinstance(body, str):
+        body = json.loads(body)
 
-    # Consultar en DynamoDB
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('tf_clientes')
+    curso_id = body["curso_id"]
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("cursos")
 
     response = table.query(
-        KeyConditionExpression=Key('cliente_id').eq(cliente_id)
+        KeyConditionExpression=Key("curso_id").eq(curso_id)
     )
 
     return {
-        'statusCode': 200,
-        'body': json.dumps(response['Items'])
+        "statusCode": 200,
+        "body": json.dumps(response["Items"])
     }
